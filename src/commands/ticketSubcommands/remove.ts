a2c39adccdbf77bf8273530f9cd2322e8f696058
Dani47 <87781904+Danii47@users.noreply.github.com>
2025-02-26 22:05:25 +0100

    ts: rename subcommand parse to typescript

--- src/commands/ticketSubcommands/remove.ts
+++ src/commands/ticketSubcommands/remove.ts
@@ -1,19 +1,22 @@
-const { EmbedBuilder, MessageFlags } = require("discord.js")
-const ActiveTicketsSchema = require("../../schemas/ActiveTicketsSchema")
+import { ChatInputCommandInteraction, GuildMember, MessageFlags, TextChannel } from "discord.js"
+import ActiveTicketsSchema from "../../schemas/ActiveTicketsSchema"
+import { RolePermission } from "../../types/CustomTypes"
 
-module.exports = {
+export default {
   name: "quitar",
-  async run(_, interaction) {
+  async run(_: any, interaction: ChatInputCommandInteraction) {
 
-    const member = interaction.options.getMember("usuario")
+    const member = interaction.options.getMember("usuario") as GuildMember
     const role = interaction.options.getRole("rol")
 
     if (!member && !role) return interaction.reply({ content: "Debes seleccionar una de las dos opciones para añadir.", flags: MessageFlags.Ephemeral })
 
-    const channelToRemoveUser = interaction.channel
+    const channelToRemoveUser = interaction.channel as TextChannel
+    if (!channelToRemoveUser) return interaction.reply({ content: "No se ha podido obtener el canal.", flags: MessageFlags.Ephemeral })
 
     if (member) {
       await channelToRemoveUser.permissionOverwrites.delete(member.id)
+      
       await interaction.reply({
         content: `📥 El usuario ${member} ha sido eliminado del ticket.`,
         flags: MessageFlags.Ephemeral
@@ -29,12 +32,12 @@ module.exports = {
       })
     }
 
-    const ActiveTicketData = await ActiveTicketsSchema.findOne({ channelId: interaction.channel.id })
+    const ActiveTicketData = await ActiveTicketsSchema.findOne({ channelId: channelToRemoveUser.id })
     if (ActiveTicketData?.ticketInfo) {
       await ActiveTicketData.updateOne({
         ticketInfo: {
           ...ActiveTicketData.ticketInfo,
-          permissions: ActiveTicketData.ticketInfo.permissions.filter((permission) => {
+          permissions: ActiveTicketData.ticketInfo.permissions.filter((permission: RolePermission) => {
             if (member && role) return permission.id !== member.id && permission.id !== role.id
             if (member) return permission.id !== member.id
             if (role) return permission.id !== role.id
