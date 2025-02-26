6d613e58fd209f610226c192ecb6f8f9f86ad9c1
Dani47 <87781904+Danii47@users.noreply.github.com>
2025-02-26 20:07:48 +0100

    ts: getTicketByCustomId parse to typescript

---
+++ src/utils/getTicketByCustomId.ts
@@ -0,0 +1,24 @@
+import { TicketInfo } from "../types/CustomTypes"
+import fs from "fs"
+
+export const getTicketByCustomId = (customId: string): TicketInfo => {
+
+  let ticketInfo
+
+  const guildTicketsFiles = fs.readdirSync("./guildTickets")
+
+  for (const guildTicketsFile of guildTicketsFiles) {
+    const guildTicketsConfig = require(`../guildTickets/${guildTicketsFile}`)
+
+    for (const guildTicketsConfigProperty in guildTicketsConfig) {
+      for (const tickets in guildTicketsConfig[guildTicketsConfigProperty].ticketOptions) {
+
+        const ticketCustomId = guildTicketsConfig[guildTicketsConfigProperty].ticketOptions[tickets].customId
+        if (customId === ticketCustomId) ticketInfo = guildTicketsConfig[guildTicketsConfigProperty].ticketOptions[tickets]
+      
+      }
+    }
+  }
+
+  return ticketInfo
+}
