a0e596a44c0043635cd215dc39a228af0669c5ea
Dani47 <87781904+Danii47@users.noreply.github.com>
2025-02-26 20:13:42 +0100

    ts: toTitleCase parse to typescript

---
+++ src/utils/toTitleCase.ts
@@ -0,0 +1,6 @@
+export const toTitleCase = (str: string): string => {
+  return str.replace(/\w\S*/g, (txt) => {
+      return txt.charAt(0).toUpperCase() + txt.substring(1).toLowerCase()
+    }
+  )
+}
