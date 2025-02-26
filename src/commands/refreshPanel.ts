500ee30e9c406ebaab8a00d8b0971dd32490bf98
Dani47 <87781904+Danii47@users.noreply.github.com>
2025-02-27 00:15:57 +0100

    fix refresh panel

--- src/commands/refreshPanel.ts
+++ src/commands/refreshPanel.ts
@@ -2,12 +2,13 @@ import { SlashCommandBuilder, EmbedBuilder, PermissionFlagsBits, MessageFlags, C
 import roles from "../configs/rolesId.json"
 import { serverName, devId } from "../configs/generalConfig.json"
 import { MyClient } from "../types/CustomTypes"
+import { Buttons } from "../constants/buttons"
 
 const NAMES_OF_TICKET_FILES = {
   [roles.ctfRoles.everyone]: "ctf",
 }
 
-module.exports = {
+export default {
   data: new SlashCommandBuilder()
     .setName("refrescarpanel")
     .setDescription("Refresca el panel que desees [DEVCOMMAND].")
@@ -20,28 +21,26 @@ module.exports = {
   async run(client: MyClient, interaction: ChatInputCommandInteraction) {
     if (!interaction.member) return interaction.reply({ content: "No se ha podido obtener la informacion de miembro.", flags: MessageFlags.Ephemeral })
     if (!interaction.guild) return interaction.reply({ content: "No se ha podido obtener la informacion de servidor.", flags: MessageFlags.Ephemeral })
-    
+
     if (typeof interaction.member.permissions === "string") return interaction.reply({ content: "No se ha podido obtener la informacion de permisos.", flags: MessageFlags.Ephemeral })
-    
+
     if (interaction.user.id != devId && !interaction.member.permissions.has(PermissionFlagsBits.Administrator)) return interaction.reply({ content: "No tienes acceso a usar este comando.", flags: MessageFlags.Ephemeral })
-    
+
     await interaction.deferReply({ flags: MessageFlags.Ephemeral })
-    
+
     const panel = interaction.options.getString("panel")
     if (!panel) return interaction.editReply({ content: "No se ha podido obtener el panel." })
 
-    const guildTicketInfo = require(`../guildTickets/${NAMES_OF_TICKET_FILES[interaction.guild.id]}.ts`)
-    
+    const { Config: guildTicketInfo } = require(`../guildTickets/${NAMES_OF_TICKET_FILES[interaction.guild.id]}.ts`)
+
     const panelInfo = guildTicketInfo[panel]
     if (!panelInfo) return interaction.editReply({ content: "El panel que has introducido no existe." })
 
     const channelToSendEmbed = client.channels.cache.get(panelInfo.openTicketChannelId) as TextChannel
     if (!channelToSendEmbed) return interaction.editReply({ content: "No se ha encontrado el canal donde enviar el panel." })
-    
+
     await channelToSendEmbed.bulkDelete(1)
 
-    const Buttons = require("../constants/buttons")
-    
     await channelToSendEmbed.send({
       components: [Buttons.create[panel]],
       embeds: [
